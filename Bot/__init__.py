13aa28b6ce4a8f05cbacf04d38a010de5293aaf6
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-10-06 10:57:21 +0530

    Update __init__.py
    Signed-off-by: Tobigod123 <134499497+Tobigod123@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -16,7 +16,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0)
 API_ID = int(environ.get('API_ID', 21755892)) #API ID
 API_HASH = environ.get('API_HASH', '86e4ee550e25c840f888feb042663545') #API HASH
 BOT_TOKEN = environ.get('BOT_TOKEN', '6387970018:AAH92XPk4loqOZ82zfp-oK5fmFgGK7-wYHA') #BOT TOKEN
-DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://neverminduff:autoanime@aytoanime.kni2mww.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp') #MONGO DB
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://1234:1234@cluster0.hvaceqn.mongodb.net/?retryWrites=true&w=majority') #MONGO DB
 OWNER_ID = int(environ.get('OWNER_ID', 6440253535)) #OWNER ID
 MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', -1001958343978))#YOUR MAIN CHANNEL ID
 ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', -1001817757798))#YOUR ARCHIVE CHANNEL
